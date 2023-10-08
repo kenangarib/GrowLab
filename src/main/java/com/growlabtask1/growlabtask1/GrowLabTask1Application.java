f68067ef31801f17a9c5e7ea1e64e4db9b0a81d7
Kenan Garib <30588599+kenangarib@users.noreply.github.com>
2023-10-08 22:07:15 +0400

    GrowLab

---
+++ src/main/java/com/growlabtask1/growlabtask1/GrowLabTask1Application.java
@@ -0,0 +1,15 @@
+package com.growlabtask1.growlabtask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowLabTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowLabTask1Application.class, args);
+        System.out.println("Sa");
+    }
+
+
+}
