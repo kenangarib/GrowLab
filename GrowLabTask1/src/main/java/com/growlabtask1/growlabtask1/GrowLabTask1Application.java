6b8160744a4e830928cc4ff61b58d5072e359429
Kenan Garib <30588599+kenangarib@users.noreply.github.com>
2023-10-08 22:01:55 +0400

    Grow Lab
    Grow Lab Spring Task 1

---
+++ GrowLabTask1/src/main/java/com/growlabtask1/growlabtask1/GrowLabTask1Application.java
@@ -0,0 +1,13 @@
+package com.growlabtask1.growlabtask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowLabTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowLabTask1Application.class, args);
+    }
+
+}
---
+++ GrowLabTask1/src/test/java/com/growlabtask1/growlabtask1/GrowLabTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growlabtask1.growlabtask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowLabTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
